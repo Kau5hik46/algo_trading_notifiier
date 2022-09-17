ff1aa694efbe7a0632e18a40e1d4dee5f4f9024b
Kaushik <kaushikkomanduri@gmail.com>
2022-09-17 23:43:21 +0530

    Added entities of account and stock market

---
+++ account.py
@@ -0,0 +1,16 @@
+class Account():
+    """
+    Class that abstracts an account
+    """
+
+    def __init__(self):
+        pass
+
+
+class TradingAccount(Account):
+    """
+    Class that inherits from an account but works as a trading account
+    """
+
+    def __init__(self):
+        pass
