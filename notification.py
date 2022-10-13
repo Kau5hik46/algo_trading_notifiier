4214c1ddc14c19fc132beadfd167344b74ec567a
lavanya-naresh <laksh112naresh@gmail.com>
2022-10-14 00:30:01 +0530

    update

--- notification.py
+++ notification.py
@@ -2,4 +2,15 @@
 objective:
 Basically a logger
 It notifies when the certain condition as stated in strategy.py is satisfied.
+
+STEPS:
+1. get the data from adapter.py for the NSE LTP
+2. using that LTP data check, make arithmetic operations with strategy.py
+3. check for the condition fulfilment.
+4. notify if condition fulfilled then timeout else timeout directly
+5. repeat loop
 """
+
+if __name__ == "__main__":
+    # data = 
+    pass
